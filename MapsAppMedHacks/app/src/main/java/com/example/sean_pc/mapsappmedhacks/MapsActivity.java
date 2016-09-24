236ad2a7db60e778cd4856a5c209f71b1f82e164
KiitanAkala <kiitan.akala@gmail.com>
2016-09-24 14:38:08 -0400

    KML updates

--- MapsAppMedHacks/app/src/main/java/com/example/sean_pc/mapsappmedhacks/MapsActivity.java
+++ MapsAppMedHacks/app/src/main/java/com/example/sean_pc/mapsappmedhacks/MapsActivity.java
@@ -1,16 +1,15 @@
 package com.example.sean_pc.mapsappmedhacks;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.kml.KmlContainer;
 import com.google.maps.android.kml.KmlLayer;
+import com.google.maps.android.kml.KmlPlacemark;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -38,23 +37,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)   {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        KmlLayer layer= new KmlLayer(googleMap,kmlInputStream, getApplicationContext());
+        KmlLayer layer;
+        KmlPlacemark pl;
+        // Add a marker in Sydney and move the camera\
         try{
+         layer= new KmlLayer(googleMap, R.raw.Pollen, getApplicationContext();
+
             layer.addLayerToMap();
         }catch(java.io.IOException e){
             layer.removeLayerFromMap();
+            System.out.println("java");
         }catch(org.xmlpull.v1.XmlPullParserException e){
             layer.removeLayerFromMap();
+            System.out.println("xmlpull");
+
         }
         for(KmlContainer container: layer.getContainers()){
             if(layer.hasContainers()){
                 googleMap.addMarker(new MarkerOptions().title(getLayoutInflater().toString()));
             }
 
+
+        }
+
+        for(KmlPlacemark placemark: layer.getPlacemarks()){
+            if(layer.hasPlacemarks()) {
+                MarkerOptions m = new MarkerOptions();
+
+//                mMap.addMarker(new MarkerOptions()).getPosition(placemark.getProperty("coordiantes"));
+                mMap.addMarker(new MarkerOptions().position());
+                mMap.
+            }
         }
         /*
         LatLng sydney = new LatLng(-34, 151);
