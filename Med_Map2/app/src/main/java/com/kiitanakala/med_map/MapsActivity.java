c6c67e9e97d62a1cbb2cc1b79d0746bb34de7283
KiitanAkala <kiitan.akala@gmail.com>
2016-09-24 19:34:59 -0400

    minor fixes

--- Med_Map2/app/src/main/java/com/kiitanakala/med_map/MapsActivity.java
+++ Med_Map2/app/src/main/java/com/kiitanakala/med_map/MapsActivity.java
@@ -1,12 +1,14 @@
-package com.example.sean_pc.mapsappmedhacks;
+package com.kiitanakala.med_map;
 
-import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,42 +35,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap)   {
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-//        mMap = googleMap;
-//
-//        KmlLayer layer = null;
-//        try {
-//            layer = new KmlLayer(googleMap, R.raw.pollenkml, getApplicationContext());
-//            for(KmlPlacemark placemark: layer.getPlacemarks()){
-//                if(layer.hasPlacemarks()) {
-//
-//                    mMap.addMarker(new MarkerOptions()).setPosition(new LatLng(placemark.getProperty("coordinates").indexOf(0), placemark.getProperty("coordinates").indexOf(1)));
-//                }
-//            }
-//            for(KmlContainer container: layer.getContainers()){
-//                if(layer.hasContainers()){
-//                    googleMap.addMarker(new MarkerOptions().title(getLayoutInflater().toString()));
-//                }
-//
-//
-//            }
-//        } catch (XmlPullParserException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-
-/*
-LatLng sydney = new LatLng(-34, 151);
-mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-*/
-
-    }
-    public static void requestGeolocation(){
-        String apiKey  ;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
